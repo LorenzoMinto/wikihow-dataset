7cc5ac2fe0c3781653af8879512c4ee2a8b21dc6
Lorenzo Minto <lorenzomnto@gmail.com>
2020-03-01 16:16:17 +0000

    - Change make file to work on wikihow dataset

--- make_datafiles.py
+++ make_datafiles.py
@@ -8,7 +8,10 @@ import json
 import tarfile
 import io
 import pickle as pkl
+import pandas as pd
+import spacy
 
+import nltk
 
 dm_single_close_quote = '\u2019' # unicode
 dm_double_close_quote = '\u201d'
@@ -16,78 +19,34 @@ dm_double_close_quote = '\u201d'
 END_TOKENS = ['.', '!', '?', '...', "'", "`", '"',
               dm_single_close_quote, dm_double_close_quote, ")"]
 
-all_train_urls = "url_lists/all_train.txt"
-all_val_urls = "url_lists/all_val.txt"
-all_test_urls = "url_lists/all_test.txt"
+wikihow_articles_tokenized = "wikihow_articles_tokenized"
 
-cnn_tokenized_stories_dir = "cnn_stories_tokenized"
-dm_tokenized_stories_dir = "dm_stories_tokenized"
-finished_files_dir = "finished_files"
+"""Add here incrementally new conditioning hypothesis, increment the final subscript"""
+def conditioning_hyp1(df):
+    return df['title'] + " $ " + df['text']
 
-# These are the number of .story files we expect there to be in cnn_stories_dir
-# and dm_stories_dir
-num_expected_cnn_stories = 92579
-num_expected_dm_stories = 219506
-
-
-def tokenize_stories(stories_dir, tokenized_stories_dir):
-    """Maps a whole directory of .story files to a tokenized version using
+def tokenize_articles(articles_csv, tokenized_stories_csv):
+    """Maps a whole csv of articles to its tokenized version using
        Stanford CoreNLP Tokenizer
     """
-    print("Preparing to tokenize {} to {}...".format(stories_dir,
-                                                     tokenized_stories_dir))
-    stories = os.listdir(stories_dir)
-    # make IO list file
-    print("Making list of files to tokenize...")
-    with open("mapping.txt", "w") as f:
-        for s in stories:
-            f.write(
-                "{} \t {}\n".format(
-                    os.path.join(stories_dir, s),
-                    os.path.join(tokenized_stories_dir, s)
-                )
-            )
-    command = ['java', 'edu.stanford.nlp.process.PTBTokenizer',
-               '-ioFileList', '-preserveLines', 'mapping.txt']
-    print("Tokenizing {} files in {} and saving in {}...".format(
-        len(stories), stories_dir, tokenized_stories_dir))
-    subprocess.call(command)
-    print("Stanford CoreNLP Tokenizer has finished.")
-    os.remove("mapping.txt")
+    print("Preparing to tokenize {} to {}...".format(articles_csv,
+                                                     tokenized_stories_csv))
 
-    # Check that the tokenized stories directory contains the same number of
-    # files as the original directory
-    num_orig = len(os.listdir(stories_dir))
-    num_tokenized = len(os.listdir(tokenized_stories_dir))
-    if num_orig != num_tokenized:
-        raise Exception(
-            "The tokenized stories directory {} contains {} files, but it "
-            "should contain the same number as {} (which has {} files). Was"
-            " there an error during tokenization?".format(
-                tokenized_stories_dir, num_tokenized, stories_dir, num_orig)
-        )
+    wikihow_df = pd.read_csv(articles_csv)
+    wikihow_df = wikihow_df[wikihow_df['text'].isna() == False]
+    # Change conditioning substructure here by redefining or adding conditioning function
+    wikihow_df['conditioning'] = conditioning_hyp1(wikihow_df)
+    wikihow_df_c1 = wikihow_df.drop(['title', 'text'], axis=1)
+    wikihow_df_c1.rename(columns={'headline': 'summary'}, inplace=True)
+
+    nltk.download('punkt')
+
+    #Remove empty lines
+    wikihow_df_c1 = wikihow_df_c1.applymap(lambda x: [" ".join(nltk.word_tokenize(line)) for line in x.split('\n') if line != ''])
+
+    wikihow_df_c1.to_csv(tokenized_stories_csv)
     print("Successfully finished tokenizing {} to {}.\n".format(
-        stories_dir, tokenized_stories_dir))
-
-
-def read_story_file(text_file):
-    with open(text_file, "r") as f:
-        # sentences are separated by 2 newlines
-        # single newlines might be image captions
-        # so will be incomplete sentence
-        lines = f.read().split('\n\n')
-    return lines
-
-
-def hashhex(s):
-    """Returns a heximal formated SHA1 hash of the input string."""
-    h = hashlib.sha1()
-    h.update(s.encode())
-    return h.hexdigest()
-
-
-def get_url_hashes(url_list):
-    return [hashhex(url) for url in url_list]
+        articles_csv, tokenized_stories_csv))
 
 
 def fix_missing_period(line):
@@ -101,91 +60,33 @@ def fix_missing_period(line):
     return line + " ."
 
 
-def get_art_abs(story_file):
-    """ return as list of sentences"""
-    lines = read_story_file(story_file)
-
-    # Lowercase, truncated trailing spaces, and normalize spaces
-    lines = [' '.join(line.lower().strip().split()) for line in lines]
-
-    # Put periods on the ends of lines that are missing them (this is a problem
-    # in the dataset because many image captions don't end in periods;
-    # consequently they end up in the body of the article as run-on sentences)
-    lines = [fix_missing_period(line) for line in lines]
-
-    # Separate out article and abstract sentences
-    article_lines = []
-    highlights = []
-    next_is_highlight = False
-    for idx, line in enumerate(lines):
-        if line == "":
-            continue # empty line
-        elif line.startswith("@highlight"):
-            next_is_highlight = True
-        elif next_is_highlight:
-            highlights.append(line)
-        else:
-            article_lines.append(line)
-
-    return article_lines, highlights
-
-
-def write_to_tar(url_file, out_file, makevocab=False):
-    """Reads the tokenized .story files corresponding to the urls listed in the
-       url_file and writes them to a out_file.
+def write_to_tar(out_file, random=True, makevocab=False):
+    """Reads the tokenized articles, splits them in train,test,val and writes them to a out_file.
     """
-    print("Making bin file for URLs listed in {}...".format(url_file))
-    url_list = [line.strip() for line in open(url_file)]
-    url_hashes = get_url_hashes(url_list)
-    story_fnames = [s+".story" for s in url_hashes]
-    num_stories = len(story_fnames)
+
+    wikihow_df = pd.read_csv(wikihow_articles_tokenized)
+    articles = wikihow_df['conditioning'].to_numpy()
+    summaries = wikihow_df['summary'].to_numpy()
 
     if makevocab:
         vocab_counter = collections.Counter()
 
-    with tarfile.open(out_file, 'w') as writer:
-        for idx, s in enumerate(story_fnames):
+    with tarfile.open(out_file+".tar", 'w') as writer:
+        articles_n = articles.size
+        for idx in range(articles_n):
             if idx % 1000 == 0:
                 print("Writing story {} of {}; {:.2f} percent done".format(
-                    idx, num_stories, float(idx)*100.0/float(num_stories)))
-
-            # Look in the tokenized story dirs to find the .story file
-            # corresponding to this url
-            if os.path.isfile(os.path.join(cnn_tokenized_stories_dir, s)):
-                story_file = os.path.join(cnn_tokenized_stories_dir, s)
-            elif os.path.isfile(os.path.join(dm_tokenized_stories_dir, s)):
-                story_file = os.path.join(dm_tokenized_stories_dir, s)
-            else:
-                print("Error: Couldn't find tokenized story file {} in either"
-                      " tokenized story directories {} and {}. Was there an"
-                      " error during tokenization?".format(
-                          s, cnn_tokenized_stories_dir,
-                          dm_tokenized_stories_dir))
-                # Check again if tokenized stories directories contain correct
-                # number of files
-                print("Checking that the tokenized stories directories {}"
-                      " and {} contain correct number of files...".format(
-                          cnn_tokenized_stories_dir, dm_tokenized_stories_dir))
-                check_num_stories(cnn_tokenized_stories_dir,
-                                  num_expected_cnn_stories)
-                check_num_stories(dm_tokenized_stories_dir,
-                                  num_expected_dm_stories)
-                raise Exception(
-                    "Tokenized stories directories {} and {}"
-                    " contain correct number of files but story"
-                    " file {} found in neither.".format(
-                        cnn_tokenized_stories_dir,
-                        dm_tokenized_stories_dir, s)
-                )
+                    idx, articles_n, float(idx)*100.0/float(articles_n)))
 
             # Get the strings to write to .bin file
-            article_sents, abstract_sents = get_art_abs(story_file)
+            article_sents = articles[idx]
+            summary_sents = summaries[idx]
 
             # Write to JSON file
             js_example = {}
-            js_example['id'] = s.replace('.story', '')
+            js_example['id'] = "{}".format(idx)
             js_example['article'] = article_sents
-            js_example['abstract'] = abstract_sents
+            js_example['summary'] = summary_sents
             js_serialized = json.dumps(js_example, indent=4).encode()
             save_file = io.BytesIO(js_serialized)
             tar_info = tarfile.TarInfo('{}/{}.json'.format(
@@ -196,8 +97,8 @@ def write_to_tar(url_file, out_file, makevocab=False):
             # Write the vocab to file, if applicable
             if makevocab:
                 art_tokens = ' '.join(article_sents).split()
-                abs_tokens = ' '.join(abstract_sents).split()
-                tokens = art_tokens + abs_tokens
+                sum_tokens = ' '.join(summary_sents).split()
+                tokens = art_tokens + sum_tokens
                 tokens = [t.strip() for t in tokens] # strip
                 tokens = [t for t in tokens if t != ""] # remove empty
                 vocab_counter.update(tokens)
@@ -207,50 +108,22 @@ def write_to_tar(url_file, out_file, makevocab=False):
     # write vocab to file
     if makevocab:
         print("Writing vocab file...")
-        with open(os.path.join(finished_files_dir, "vocab_cnt.pkl"),
+        with open("vocab_cnt.pkl",
                   'wb') as vocab_file:
             pkl.dump(vocab_counter, vocab_file)
         print("Finished writing vocab file")
 
-
-def check_num_stories(stories_dir, num_expected):
-    num_stories = len(os.listdir(stories_dir))
-    if num_stories != num_expected:
-        raise Exception(
-            "stories directory {} contains {} files"
-            " but should contain {}".format(
-                stories_dir, num_stories, num_expected)
-        )
-
-
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("USAGE: python make_datafiles.py"
-              " <cnn_stories_dir> <dailymail_stories_dir>")
+    if len(sys.argv) != 2:
+        print("USAGE: python make_wh_datafiles.py"
+              " <wikihow_articles_csv>")
         sys.exit()
-    cnn_stories_dir = sys.argv[1]
-    dm_stories_dir = sys.argv[2]
-
-    # Check the stories directories contain the correct number of .story files
-    check_num_stories(cnn_stories_dir, num_expected_cnn_stories)
-    check_num_stories(dm_stories_dir, num_expected_dm_stories)
-
-    # Create some new directories
-    if not os.path.exists(cnn_tokenized_stories_dir):
-        os.makedirs(cnn_tokenized_stories_dir)
-    if not os.path.exists(dm_tokenized_stories_dir):
-        os.makedirs(dm_tokenized_stories_dir)
-    if not os.path.exists(finished_files_dir):
-        os.makedirs(finished_files_dir)
+    wh_articles_csv = sys.argv[1]
 
     # Run stanford tokenizer on both stories dirs,
     # outputting to tokenized stories directories
-    tokenize_stories(cnn_stories_dir, cnn_tokenized_stories_dir)
-    tokenize_stories(dm_stories_dir, dm_tokenized_stories_dir)
+    tokenize_articles(wh_articles_csv, wikihow_articles_tokenized)
 
     # Read the tokenized stories, do a little postprocessing
     # then write to bin files
-    write_to_tar(all_test_urls, os.path.join(finished_files_dir, "test.tar"))
-    write_to_tar(all_val_urls, os.path.join(finished_files_dir, "val.tar"))
-    write_to_tar(all_train_urls, os.path.join(finished_files_dir, "train.tar"),
-                 makevocab=True)
+    write_to_tar(wikihow_articles_tokenized, random=True, makevocab=True)
